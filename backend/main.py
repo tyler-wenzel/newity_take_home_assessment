c34929f7d56ce74ff064364a9a9aba0478c34932
tyler-wenzel <tyler.gh3@gmail.com>
2026-09-13 11:36:00 -0400

    update cors to accept request from front end

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controller import router
 
 app = FastAPI(title="Document Checklist")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
